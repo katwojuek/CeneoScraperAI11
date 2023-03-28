e79b18e21fff460e0f805df19d6fbd6b16f8e698
katwojuek <wojcikk@uek.krakow.pl>
2023-03-28 17:14:57 +0200

    2023-03-28 KW

---
+++ scraper.py
@@ -0,0 +1,7 @@
+import requests
+
+# product_code = input("Please enter product code: ")
+product_code = "129901214"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = requests.get(url)
+print(response.status_code)
